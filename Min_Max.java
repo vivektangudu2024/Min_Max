ed059159d3a6932508a99e125548354e68979d81
vivektangudu2024 <tanguduvivek2003.com>
2023-12-11 00:31:14 +0530

    Added UC3

--- Min_Max.java
+++ Min_Max.java
@@ -19,15 +19,15 @@ public class Min_Max {
 
     public static void main(String[] args) {
         // Test Case 1: Max at 1st position
-        Float max1 = findMax(5.2f, 3.1f, 2.9f);
+        String max1 = findMax("Banana", "Apple", "Orange");
         System.out.println("Test Case 1 - Max: " + max1);
 
         // Test Case 2: Max at 2nd position
-        Float max2 = findMax(1.0f, 8.5f, 4.3f);
+        String max2 = findMax("Elephant", "Zebra", "Lion");
         System.out.println("Test Case 2 - Max: " + max2);
 
         // Test Case 3: Max at 3rd position
-        Float max3 = findMax(6.7f, 1.2f, 9.0f);
+        String max3 = findMax("Java", "Python", "C++");
         System.out.println("Test Case 3 - Max: " + max3);
     }
 }
