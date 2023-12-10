60866451cfe1bc47d796bd56461e36c12b83fd62
vivektangudu2024 <tanguduvivek2003.com>
2023-12-11 00:38:57 +0530

    Added Refactored common generic method

--- Min_Max.java
+++ Min_Max.java
@@ -2,7 +2,7 @@ package com.day8;
 
 import java.util.*;
 public class Min_Max {
-
+    //@desc:generic METHOD using data type
     public static <T extends Comparable<T>> T findMax(T x, T y, T z) {
         T max = x;
 
@@ -19,15 +19,15 @@ public class Min_Max {
 
     public static void main(String[] args) {
         // Test Case 1: Max at 1st position
-        String max1 = findMax("Banana", "Apple", "Orange");
-        System.out.println("Test Case 1 - Max: " + max1);
+        Integer maxInt = findMax(5, 3, 2);
+        System.out.println("Test Case 1 - Max Integer: " + maxInt);
 
         // Test Case 2: Max at 2nd position
-        String max2 = findMax("Elephant", "Zebra", "Lion");
-        System.out.println("Test Case 2 - Max: " + max2);
+        Float maxFloat = findMax(1.0f, 8.5f, 4.3f);
+        System.out.println("Test Case 2 - Max Float: " + maxFloat);
 
         // Test Case 3: Max at 3rd position
-        String max3 = findMax("Java", "Python", "C++");
-        System.out.println("Test Case 3 - Max: " + max3);
+        String maxString = findMax("Java", "Python", "C++");
+        System.out.println("Test Case 3 - Max String: " + maxString);
     }
 }
