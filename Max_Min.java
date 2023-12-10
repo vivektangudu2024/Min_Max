588d540a729496bc36fa7f7daee18746d8eaaaa1
vivektangudu2024 <tanguduvivek2003.com>
2023-12-11 01:16:01 +0530

    Added UC6

--- Max_Min.java
+++ Max_Min.java
@@ -5,28 +5,28 @@ import java.util.*;
 //@desc:generic Class using data type
 class MaxFinder<T extends Comparable<T>> {
 
-    private T x;
-    private T y;
-    private T z;
+    private T[] values;
 
-    public MaxFinder(T x, T y, T z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public MaxFinder(T... values) {
+        this.values = values;
     }
 
     public T findMax() {
-        T max = x;
-
-        if (y.compareTo(max) > 0) {
-            max = y;
+        if (values == null || values.length == 0) {
+            return null; // No elements to compare
         }
 
-        if (z.compareTo(max) > 0) {
-            max = z;
+        Arrays.sort(values);
+        return values[values.length - 1];
+    }
+
+    public static <T extends Comparable<T>> T testMaximum(T... values) {
+        if (values == null || values.length == 0) {
+            return null; // No elements to compare
         }
 
-        return max;
+        Arrays.sort(values);
+        return values[values.length - 1];
     }
 }
 
@@ -36,20 +36,12 @@ class MaxFinder<T extends Comparable<T>> {
 
 
         public static void main(String[] args) {
-            // Test Case 1: Max at 1st position using Generic Class
-            MaxFinder<Integer> integerMaxFinder = new MaxFinder<>(5, 3, 2);
+            // Test Case 1: Max using Generic Class with more than three parameters
+            MaxFinder<Integer> integerMaxFinder = new MaxFinder<>(5, 3, 2, 8, 1);
             System.out.println("Test Case 1 - Max Integer (Generic Class): " + integerMaxFinder.findMax());
 
-            // Test Case 2: Max at 2nd position using Generic Class
-            MaxFinder<Float> floatMaxFinder = new MaxFinder<>(1.0f, 8.5f, 4.3f);
-            System.out.println("Test Case 2 - Max Float (Generic Class): " + floatMaxFinder.findMax());
-
-            // Test Case 3: Max at 3rd position using Generic Class
-            MaxFinder<String> stringMaxFinder = new MaxFinder<>("Java", "Python", "C++");
-            System.out.println("Test Case 3 - Max String (Generic Class): " + stringMaxFinder.findMax());
-
-            // New Test Case using Generic Class
-            MaxFinder<Double> doubleMaxFinder = new MaxFinder<>(2.5, 3.8, 1.2);
-            System.out.println("New Test Case - Max Double (Generic Class): " + doubleMaxFinder.findMax());
+            // Test Case 2: Max using static method with more than three parameters
+            Double maxDouble = MaxFinder.testMaximum(2.5, 3.8, 1.2, 5.6, 4.0);
+            System.out.println("Test Case 2 - Max Double (Static Method): " + maxDouble);
         }
     }
